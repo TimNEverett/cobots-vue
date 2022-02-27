069e066c92b0d19ffd81bfaaeee75f72079cb982
Tim Everett <tim.neil.everett@gmail.com>
2022-02-27 11:40:09 -0400

    feat: create wallet button component, add wallet button to home page

---
+++ src/components/walletButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col items-center space-y-4">
+    <button class=" w-40 rounded-full border-2 border-zinc-300 px-4 py-2 font-black">
+      {{ addressTruncated }}    
+    </button>
+    <div class="uppercase underline font-black text-sky-400">View my bots</div>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'walletButton',
+   data() {
+     return {
+       address: '0x25522B4FD92cEa7749227EC2a4e4e3F640b58ae5'
+     }
+   },
+   computed: {
+     addressTruncated() {
+       let start = this.address.substring(0, 4)
+       let end = this.address.substring(this.address.length - 4, this.address.length)
+       return `${start}...${end}`
+     }
+   }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="h-80 lg:h-[calc(100vh-5rem)] flex flex-col justify-between">
     <div class="flex flex-col justify-center items-center h-full">
+      <div class="absolute right-4 top-24">
+        <wallet-button />
+      </div>
+      
       <router-view></router-view>
     </div>
     <img 
@@ -11,10 +15,12 @@
 </template>
 <script>
 import InfoSection from "@/components/InfoSection.vue"
+import WalletButton from "@/components/walletButton.vue"
 export default {
    name: 'Home',
    components: {
-     InfoSection
+     InfoSection,
+     WalletButton
    }
 }
 </script>
