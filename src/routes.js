73f03ad1c81d7c11d42b19ccd250e4646c039841
Tim Everett <tim.neil.everett@gmail.com>
2022-02-27 11:37:45 -0400

    feat: add placeholder function for dynamic redirect based on wallet connected status

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,17 @@ import NotFound from './views/NotFound.vue'
 import ConnectWalletPanel from './components/ConnectWalletPanel.vue'
 import MintPanel from './components/MintPanel.vue'
 
+const connected = false;
+
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
   { 
     path: '/', 
-    component: Home, 
+    component: Home,
+    redirect: (to) => {
+      if(connected) return '/mint'
+      return '/connect'
+    },
     meta: { 
       title: 'Home' 
     },
