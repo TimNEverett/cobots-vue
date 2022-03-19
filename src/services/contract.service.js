24c134b626d66df10062d59727967bd2881bcb90
Tim Everett <tim.neil.everett@gmail.com>
2022-03-19 15:59:02 -0300

    add favicon, make network variable and env var

--- src/services/contract.service.js
+++ src/services/contract.service.js
@@ -2,10 +2,13 @@ import { ethers } from "ethers";
 import abi from "./abi.json";
 import store from "@/store";
 
-export var provider = new ethers.getDefaultProvider("homestead", {
-  etherscan: import.meta.env.VITE_ETHERSCAN_ID,
-  infura: import.meta.env.VITE_INFURA_ID,
-});
+export var provider = new ethers.getDefaultProvider(
+  import.meta.env.VITE_NETWORK,
+  {
+    etherscan: import.meta.env.VITE_ETHERSCAN_ID,
+    infura: import.meta.env.VITE_INFURA_ID,
+  }
+);
 
 export var contract = new ethers.Contract(
   import.meta.env.VITE_CONTRACT_ADDRESS,
